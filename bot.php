8bc963a769efd523f7349568787bfd5f3fe69471
Nopanut Noojit <step_nut@hotmail.com>
2016-10-18 11:36:57 +0700

    test bot

--- bot.php
+++ bot.php
@@ -17,11 +17,17 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			if ($text == 'รักพี่นัท') {
+			$messages = [
+				'type' => 'text',
+				'text' => 'รักเกตเหมือนกัน'
+			];			
+			}else {
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
-
+			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
